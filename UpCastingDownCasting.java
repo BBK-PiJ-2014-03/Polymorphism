799186d018257bd5b59cb54fa44847c6814d4612
igalna <igalna@gmail.com>
2014-11-03 14:27:37 +0000

    2.2

--- UpCastingDownCasting.java
+++ UpCastingDownCasting.java
@@ -2,13 +2,26 @@ public class UpCastingDownCasting {
     
     public static void main(String[] args) {
         
-        MobilePhone myPhone = new SmartPhone("Nokia");
+        UpCastingDownCasting test = new UpCastingDownCasting();
         
-        System.out.println(myPhone);
+        Phone userPhone = new SmartPhone("Nokia");
+        
+        /* System.out.println(myPhone);
         
         myPhone.playGame("Pokemon");
         myPhone.browseWeb("www.bbc.co.uk");
         myPhone.call("02020");
-        myPhone.call("00123456789");
+        myPhone.call("00123456789"); */
+        
+        test.testPhone(userPhone);
+    }
+    
+
+
+    public void testPhone(Phone newPhone) {
+        /* newPhone.playGame("The Sims"); */
+        /* newPhone.browseWeb("www.bbc.co.uk"); */
+        newPhone.call("3423");
+        newPhone.call("00987654321");
     }
 }
