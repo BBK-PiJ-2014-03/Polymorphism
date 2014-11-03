13cb1cd3193643ade092acda6225398d3a0499d2
igalna <igalna@gmail.com>
2014-11-03 14:52:48 +0000

    Third Commit

--- UpCastingDownCasting.java
+++ UpCastingDownCasting.java
@@ -6,6 +6,8 @@ public class UpCastingDownCasting {
         
         Phone userPhone = new SmartPhone("Nokia");
         
+        MobilePhone newMob = new MobilePhone("Samsung");
+        
         /* System.out.println(myPhone);
         
         myPhone.playGame("Pokemon");
@@ -13,14 +15,22 @@ public class UpCastingDownCasting {
         myPhone.call("02020");
         myPhone.call("00123456789"); */
         
+       /*  test.testPhone(userPhone); */
+        test.testPhone(newMob);
         test.testPhone(userPhone);
     }
     
 
 
     public void testPhone(Phone newPhone) {
-        /* newPhone.playGame("The Sims"); */
-        /* newPhone.browseWeb("www.bbc.co.uk"); */
+        
+        if (newPhone instanceof MobilePhone) {
+            ((MobilePhone)newPhone).printGame("The Sims");
+        }
+        
+        if (newPhone instanceof SmartPhone) {
+            ((SmartPhone) newPhone).browseWeb("www.bbc.co.uk");
+        }
         newPhone.call("3423");
         newPhone.call("00987654321");
     }
